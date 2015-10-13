341fa420002da4543da1d742b0c80e76f2d4ecca
Mike <kjyoung@ualberta.ca>
2015-10-13 13:17:35 -0600

    choose random opening to improve variation

--- stochastic_data/gen_games.py
+++ stochastic_data/gen_games.py
@@ -2,6 +2,7 @@ import argparse
 import random
 from program import Program
 import sys
+import random
 
 def print_game(game):
 	for move in game:
@@ -22,18 +23,23 @@ wolve = Program("../src/wolve/./wolve 2>/dev/null", False)
 wolve.sendCommand("param_wolve temperature "+str(args.temperature))
 wolve.sendCommand("boardsize "+str(args.boardsize))
 
+moves = [chr(ord('a')+x)+str(y+1) for x in range(args.boardsize) for y in range(args.boardsize)]
+
 for i in range(args.games):
 	wolve.sendCommand("clear_board")
+	opening = random.choice(moves)
 	game = []
+	wolve.sendCommand("play black "+opening)
+	game.append(opening)
 	while(True):
-		move = wolve.sendCommand("genmove black").strip()
+		move = wolve.sendCommand("genmove white").strip()
 		if(str(move) != "resign"):
 			game.append(move)
 		else:
 			print_game(game)
 			sys.stdout.flush()
 			break
-		move = wolve.sendCommand("genmove white").strip()
+		move = wolve.sendCommand("genmove black").strip()
 		if(str(move) != "resign"):
 			game.append(move)
 		else:
