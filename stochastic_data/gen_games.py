f87e170cd933640a8f41893fd8ee984ba5688f2e
Mike <kjyoung@ualberta.ca>
2015-10-14 19:56:14 -0600

    added code to restart program if it dies for some reason

--- stochastic_data/gen_games.py
+++ stochastic_data/gen_games.py
@@ -26,26 +26,32 @@ wolve.sendCommand("boardsize "+str(args.boardsize))
 moves = [chr(ord('a')+x)+str(y+1) for x in range(args.boardsize) for y in range(args.boardsize)]
 
 for i in range(args.games):
-	wolve.sendCommand("clear_board")
-	opening = random.choice(moves)
-	game = []
-	wolve.sendCommand("play black "+opening)
-	game.append(opening)
-	while(True):
-		move = wolve.sendCommand("genmove white").strip()
-		if(str(move) != "resign"):
-			game.append(move)
-		else:
-			print_game(game)
-			sys.stdout.flush()
-			break
-		move = wolve.sendCommand("genmove black").strip()
-		if(str(move) != "resign"):
-			game.append(move)
-		else:
-			print_game(game)
-			sys.stdout.flush()
-			break
+	try:
+		wolve.sendCommand("clear_board")
+		opening = random.choice(moves)
+		game = []
+		wolve.sendCommand("play black "+opening)
+		game.append(opening)
+		while(True):
+			move = wolve.sendCommand("genmove white").strip()
+			if(str(move) != "resign"):
+				game.append(move)
+			else:
+				print_game(game)
+				sys.stdout.flush()
+				break
+			move = wolve.sendCommand("genmove black").strip()
+			if(str(move) != "resign"):
+				game.append(move)
+			else:
+				print_game(game)
+				sys.stdout.flush()
+				break
+	#reinitialize if bad things happen
+	except Program.Died:
+		wolve = Program("../src/wolve/./wolve 2>/dev/null", False)
+		wolve.sendCommand("param_wolve temperature "+str(args.temperature))
+		wolve.sendCommand("boardsize "+str(args.boardsize))
 
 
 
